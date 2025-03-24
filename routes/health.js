df84d2ae5aa7c0f2b859d7544508a17f10917d99
LuLuLuann <lu.akaydin@gmail.com>
2025-03-23 22:41:36 -0400

    all routes updated with CRUD

--- routes/health.js
+++ routes/health.js
@@ -2,13 +2,73 @@
 
 // router is like a mini app
 import { Router } from "express";
+import Health from "../models/Health.js"
 
 export const healthRouter = new Router();
 
 // move to folder called "controllers" if you want to scale
-healthRouter.get("/", (req, res) => {
-    res.status(200).json({
-        "status":"ok"
-    })
+// healthRouter.get("/", (req, res) => {
+//     res.status(200).json({
+//         "status":"ok"
+//     })
+// });
+
+// C
+healthRouter.post("/health", async (req, res) => {
+  try {
+    const health = new Health(req.body);
+    await health.save();
+    res.status(201).json(health);
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({ message: e.message });
+  }
 });
 
+// R
+// get all the health records
+healthRouter.get("/health/:id", async (req, res) => {
+  try {
+    const health = await Health.find({});
+    res.json(health)
+  } catch (e) {
+    res.status(200)
+  }
+})
+
+//get a single user's health
+healthRouter.get("/health/:id", async (req, res) => {
+  // res.status(200).json({
+  //     "status":"ok"
+  try {
+    const health = await Health.findById(req.params.id)
+    if (!health) return res.status(404).json({ message: "User health not found" })
+    res.json(health);
+
+  } catch (e) {
+    res.status(500).json({ message: e.message }) // not connecting to database
+  }
+});
+
+// U
+healthRouter.patch("/health/:id", async (req, res) => {
+  try {
+    const updatedHealth = await Health.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedHealth) return res.status(404).json({ message: "User health not found" });
+    res.json(updatedHealth);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message }); // or 400?
+  }
+})
+
+healthRouter.delete("/:id", async (req, res) => {
+  try {
+    const deletedHealth = await Health.findByIdAndDelete(req.params.id);
+    if (!deletedHealth) return res.status(404).json({ message: "User health not found" });
+    res.json({ message: "User health deleted successfully" });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message }); // or 400?
+  }
+})
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,1 +1,68 @@
 // create / add / delete restaurants
+
+// uses the schema from Mongoose -- if there are models it is Mongoose -- mongoose to a try/catch
+
+import { Router } from "express";
+import User from "../models/Restaurants.js"; // imports schema--> model
+
+export const restaurantsRouter = new Router();
+
+// C
+restaurantsRouter.post("/restaurants", async (req, res) => {
+  try {
+    const restaurant = new Restaurant(req.body);
+    await restaurant.save();
+    res.status(201).json(restaurant);
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({ message: e.message });
+  }
+});
+
+// R
+// get all
+restaurantsRouter.get("/restaurants", async (req, res) => {
+  try {
+    const restaurant = await Restaurant.find({});
+    res.json(restaurant)
+  } catch (e) {
+    res.status(200)
+  }
+})
+
+//get a single user
+restaurantsRouter.get("/restaurants/:id", async (req, res) => {
+  // res.status(200).json({
+  //     "status":"ok"
+  try {
+    const restaurant = await Restaurant.findById(req.params.id)
+    if (!restaurant) return res.status(404).json({ message: "Restaurant not found" })
+    res.json(user);
+
+  } catch (e) {
+    res.status(500).json({ message: e.message }) // not connecting to database
+  }
+});
+
+// U
+restaurantsRouter.patch("/restaurants/:id", async (req, res) => {
+  try {
+    const updatedRestaurant = await Restaurant.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedRestaurant) return res.status(404).json({ message: "Restaurant not found" });
+    res.json(updatedRestaurant);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message }); // or 400?
+  }
+})
+
+restaurantsRouter.delete("restaurants/:id", async (req, res) => {
+  try {
+    const deletedRestaurant = await Restaurant.findByIdAndDelete(req.params.id);
+    if (!deletedRestaurant) return res.status(404).json({ message: "Restaurant not found" });
+    res.json({ message: "Restaurant deleted successfully" });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: e.message }); // or 400?
+  }
+})
